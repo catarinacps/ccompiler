733544e2fc129c69d5973141a6133a762d1e1d65
Henrique Silva <hcpsilva@inf.ufrgs.br>
2020-08-29 03:42:40 -0300

    some initial files

---
+++ include/lexer/lexer.h
@@ -0,0 +1,22 @@
+/** lexer.h - Lexer specification header
+ *
+ * @file
+ * @author Henrique Silva <hcpsilva@inf.ufrgs.br>
+ * @author Bernardo Hummes <bhflores@inf.ufrgs.br>
+ *
+ * @section LICENSE
+ *
+ * This file is subject to the terms and conditions defined in the file
+ * 'LICENSE', which is part of this source code package.
+ *
+ * @section DESCRIPTION
+ *
+ *
+ */
+
+#ifndef _LEXER_H_
+#define _LEXER_H_
+
+#include <stdio.h>
+
+#endif /* _LEXER_H_ */
