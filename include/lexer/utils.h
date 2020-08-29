91f510e9b21a4d4eaa506ec476601f1a02860730
Henrique Silva <hcpsilva@inf.ufrgs.br>
2020-08-29 15:06:12 -0300

    missing doc line

--- include/lexer/utils.h
+++ include/lexer/utils.h
@@ -6,7 +6,7 @@
  *
  * @section LICENSE
  *
-
+ * This file is subject to the terms and conditions defined in the file
  * 'LICENSE', which is part of this source code package.
  *
  * @section DESCRIPTION
