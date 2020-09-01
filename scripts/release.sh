79512d489344ba4eb15dd56a48434f891c5621f8
Henrique Silva <hcpsilva@inf.ufrgs.br>
2020-09-01 00:24:32 -0300

    removed CRs from makefile and added release script

---
+++ scripts/release.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+## release.sh
+#
+# Copyright: (C) 2020 Henrique Silva
+#
+# Author: Henrique Silva <hcpsilva@inf.ufrgs.br>
+#
+# License: GNU General Public License version 3, or any later version
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# Commentary:
+#
+# Creates a .tar.gz aggregate of all the project files in the directory
+# from where it was called.
+
+root=$(dirname $(dirname $(readlink -f "$0")))
+
+pushd $root
+
+version=$(awk '/RELEASE :=/ {print $3}' Makefile)
+
+tar czf "$version.tgz" *
+
+popd
