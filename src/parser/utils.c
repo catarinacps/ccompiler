4db53660cd4c4697633e4b960448e510bdc169de
Henrique Silva <hcpsilva@inf.ufrgs.br>
2020-09-20 22:17:16 -0300

    added extra file info comment headers

--- src/parser/utils.c
+++ src/parser/utils.c
@@ -1,9 +1,8 @@
 /** utils.c
  *
- * @copyright (C) 2020 Henrique Silva
- *
- *
+ * @file
  * @author Henrique Silva <hcpsilva@inf.ufrgs.br>
+ * @author Bernardo Hummes <bhflores@inf.ufrgs.br>
  *
  * @section LICENSE
  *
