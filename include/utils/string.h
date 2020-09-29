8cb45a5b244a0117ff3e7c35369dbbb739ff1bc3
Henrique Silva <hcpsilva@inf.ufrgs.br>
2020-09-29 13:50:55 -0300

    added string manipulation helper functions

---
+++ include/utils/string.h
@@ -0,0 +1,28 @@
+/** string.h
+ *
+ * @copyright (C) 2020 Henrique Silva
+ *
+ *
+ * @author Henrique Silva <hcpsilva@inf.ufrgs.br>
+ *
+ * @section LICENSE
+ *
+ * This file is subject to the terms and conditions defined in the file
+ * 'LICENSE', which is part of this source code package.
+ *
+ * @section DESCRIPTION
+ *
+ *
+ */
+
+#ifndef _STRING_H_
+#define _STRING_H_
+
+#include <string.h>
+#include <stdlib.h>
+
+#include "utils/debug.h"
+
+char* cc_expand_escape_codes(const char* input, size_t limit);
+
+#endif /* _STRING_H_ */
---
+++ src/utils/string.c
@@ -0,0 +1,62 @@
+/** string.c
+ *
+ * @copyright (C) 2020 Henrique Silva
+ *
+ *
+ * @author Henrique Silva <hcpsilva@inf.ufrgs.br>
+ *
+ * @section LICENSE
+ *
+ * This file is subject to the terms and conditions defined in the file
+ * 'LICENSE', which is part of this source code package.
+ */
+
+#include "utils/string.h"
+
+char* cc_expand_escape_codes(const char* input, size_t limit)
+{
+    if (limit < 2) {
+        D_PRINTF("tried to expand escape codes of string of size %u\n", limit);
+        return NULL;
+    }
+
+    char* new_string = (char*)calloc(limit + 1, sizeof(char));
+
+    /* ESC_SEQ [abfnrtv\\\"\'] */
+
+    for (unsigned int i = 0, j = 0; i < limit; i++, j++) {
+        if (input[i] == '\\') {
+            switch (input[i + 1]) {
+            case 'a':
+                new_string[j] = '\a';
+                break;
+            case 'b':
+                new_string[j] = '\b';
+                break;
+            case 'f':
+                new_string[j] = '\f';
+                break;
+            case 'n':
+                new_string[j] = '\n';
+                break;
+            case 'r':
+                new_string[j] = '\r';
+                break;
+            case 't':
+                new_string[j] = '\t';
+                break;
+            case 'v':
+                new_string[j] = '\v';
+                break;
+            default:
+                new_string[j] = input[i + 1];
+            }
+
+            i++;
+        } else {
+            new_string[j] = input[i];
+        }
+    }
+
+    return new_string;
+}
