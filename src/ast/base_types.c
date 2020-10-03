7b9d380ad6e094ab6a4a342b57368034786cf078
Henrique Silva <hcpsilva@inf.ufrgs.br>
2020-10-03 01:09:56 -0300

    added base types to ast module
    in an effort to clean up the ast header

---
+++ src/ast/base_types.c
@@ -0,0 +1,27 @@
+/** base_types.c
+ *
+ * @copyright (C) 2020 Henrique Silva
+ *
+ *
+ * @author Henrique Silva <hcpsilva@inf.ufrgs.br>
+ *
+ * @section LICENSE
+ *
+ * This file is subject to the terms and conditions defined in the file
+ * 'LICENSE', which is part of this source code package.
+ */
+
+#include "ast/base_types.h"
+
+void cc_invert_number_literal(cc_literal_data_t* num_literal, cc_expression_t expr, cc_type_t type)
+{
+    /* do nothing if the given literal is of the incorrect type */
+    if (expr == cc_expr_un_sign_neg) {
+        if (type == cc_type_int)
+            num_literal->integer = -num_literal->integer;
+        else
+            num_literal->floating = -num_literal->floating;
+    }
+
+    return;
+}
