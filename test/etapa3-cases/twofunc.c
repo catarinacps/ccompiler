ec592a830841ef8131c7c7f7136b27dd6bc37fdb
Henrique Silva <hcpsilva@inf.ufrgs.br>
2020-10-04 22:09:06 -0300

    added a test case

---
+++ test/etapa3-cases/twofunc.c
@@ -0,0 +1,59 @@
+/** twofunc.c
+ *
+ * @copyright (C) 2020 Henrique Silva
+ *
+ *
+ * @author Henrique Silva <hcpsilva@inf.ufrgs.br>
+ *
+ * @section LICENSE
+ *
+ * This file is subject to the terms and conditions defined in the file
+ * 'LICENSE', which is part of this source code package.
+ */
+
+int vetor_global[20];
+
+int main() {
+    int teste;
+
+    teste = (4 % 2) == 0 ? 3 * ( *a + *b / 3) : &algo;
+
+    char primeiro, segundo <= 'c', terceiro;
+
+    string crazy, tchau;
+
+    {
+        for (i = 0 : i <= 50 : i = 1 + i) {
+            print(crazy, mais_um - 1);
+        };
+
+        print(tchau);
+    };
+
+    vetor_global[5] = -1;
+
+    return teste;
+}
+
+int outra_main(string parametro) {
+    int preciso, de, muitas, variaveis <= 2, asvezesinicializo, ounao;
+
+    for (preciso = 0 : preciso <= 5 : preciso = preciso + 1 - vetor_global[4]) {
+        print(de, muitas - 4 * (parametro == 0 ? 1 : 0));
+        main();
+    };
+
+    de << 10;
+
+    vetor_global[0] << 7;
+
+    string oba <= "oba", ahnao <= "ohno";
+
+    if (preciso == 6) {
+        print(oba);
+    } else {
+        print(ahnao);
+    };
+
+    return false;
+}
